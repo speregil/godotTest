600422d38e8309b5f08594fa1f5296b062858a89
speregil <s.gilparga@gmail.com>
2024-03-18 14:04:55 +1300

    Walking animation system

--- characters/hero/hero.gd
+++ characters/hero/hero.gd
@@ -5,9 +5,13 @@ extends CharacterBody2D
 #-------------------------------------------------------------------------------
 
 # Movement speed of this character
-@export var speed = 400
+@export var speed = 400;
+
+#-------------------------------------------------------------------------------
+# Engine Functions
+#-------------------------------------------------------------------------------
 func _ready():
-	$AnimatedSprite2D.animation = "walk";
+	$AnimatedSprite2D.animation = "idle";
 	
 func _physics_process(delta):
 	var input_direction = Vector2(
@@ -18,8 +22,19 @@ func _physics_process(delta):
 	velocity = input_direction * speed;
 	
 	move_and_slide();
-	
+	move_animation();
+
+#-------------------------------------------------------------------------------
+# Behaviour Functions
+#-------------------------------------------------------------------------------
+
+func move_animation():
 	if velocity.length() > 0:
-		$AnimatedSprite2D.play()
+		if velocity.x != 0:
+			$AnimatedSprite2D.animation = "walk-right";
+			$AnimatedSprite2D.flip_h = velocity.x < 0;
+		elif velocity.y != 0:
+			$AnimatedSprite2D.animation = "walk-up";
+		$AnimatedSprite2D.play();
 	else:
-		$AnimatedSprite2D.stop()
+		$AnimatedSprite2D.stop();
